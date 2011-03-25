fe16d531fab4c23ae5dd054eabce4031387fd43f
Arman Uguray <arman.uguray@gmail.com>
2011-03-25 16:21:34 -0400

    made a minor change in the signature

--- src/GLFileLoader.js
+++ src/GLFileLoader.js
@@ -7,21 +7,36 @@
  
  /*
  
-    // returns a texture id
-    GLuint loadTexture(string path);
+    /*
+     * Loads a texture into memory
+     *
+     * @param gl: WebGL object
+     * @param path: path to the image containing the texture
+     * @return texture id
+    GLuint loadTexture(gl, path);
     
     /*
+     * @param gl: WebGL object
+     * @param path: path to the shader file
+     * @param type: VERT or FRAG
+     * @return shader object
+     *
+    GLuint loadShaderFromPath(gl, type, path);
+    
+    /*
+     * @param gl: WebGL object
      * @param src: The shader source code
      * @param type: VERT or FRAG
      * @return shader object
      *
-    GLuint loadShader(string path);
+    GLuint loadShader(gl, type, src);
     
     /*
      * Links the given list of shader objects
      * and creates a shader program.
      * 
-     * @param array of shaders
+     * @param gl: WebGL object
+     * @param shaders: array of shader objects
      * @return shader program object
-     GLuint createShaderProgram(array shaders);
+     GLuint createShaderProgram(gl, shaders);
 */
